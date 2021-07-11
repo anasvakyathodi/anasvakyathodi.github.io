3c67334e5a678fa90558e6c1d5c9746452449cb9
anasvakyathodi <anasvakyathodi@gmail.com>
2021-07-11 17:06:44 +0530

    message sent update

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -21,33 +21,37 @@ const toggleWork = () => {
 };
 
 const handleSubmit = () => {
-    try {
-        let form = document.getElementsByName("messageForm");
-        let message = "";
-        message += "name: " + form[0].elements["name"].value;
-        message += "\temail: " + form[0].elements["email"].value;
-        message += "\tsubject: " + form[0].elements["subject"].value;
-        message += "\tmessage: " + form[0].elements["message"].value;
-        form[0].elements["name"].value = "";
-        form[0].elements["email"].value = "";
-        form[0].elements["subject"].value = "";
-        form[0].elements["message"].value = "";
-        form[0].elements["submit-btn"].value = "Senting Message";
-        form[0].elements["name"].disabled = true;
-        form[0].elements["email"].disabled = true;
-        form[0].elements["subject"].disabled = true;
-        form[0].elements["message"].disabled = true;
-        form[0].elements["submit-btn"].disabled = true;
-        let endpoint = "https://api.callmebot.com/whatsapp.php?phone=+919656551078&apikey=252408&text="+message;
-        fetch(endpoint).then((res)=>{
-            console.log(res)
-            form[0].elements["submit-btn"].value = "Message Sent";
-        }).catch((err)=>{
-            console.log(err)
-        })
-        return false;
-    } catch (error) {
-        console.log(error)
-        return false
-    }
+  try {
+    let form = document.getElementsByName("messageForm");
+    let message = "";
+    message += "name: " + form[0].elements["name"].value;
+    message += "\temail: " + form[0].elements["email"].value;
+    message += "\tsubject: " + form[0].elements["subject"].value;
+    message += "\tmessage: " + form[0].elements["message"].value;
+    form[0].elements["name"].value = "";
+    form[0].elements["email"].value = "";
+    form[0].elements["subject"].value = "";
+    form[0].elements["message"].value = "";
+    form[0].elements["submit-btn"].value = "Senting Message";
+    form[0].elements["name"].disabled = true;
+    form[0].elements["email"].disabled = true;
+    form[0].elements["subject"].disabled = true;
+    form[0].elements["message"].disabled = true;
+    form[0].elements["submit-btn"].disabled = true;
+    let endpoint =
+      "https://api.callmebot.com/whatsapp.php?phone=+919656551078&apikey=252408&text=" +
+      message;
+    fetch(endpoint, { mode: "no-cors" })
+      .then((res) => {
+        console.log(res);
+        form[0].elements["submit-btn"].value = "Message Sent";
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    return false;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 };
